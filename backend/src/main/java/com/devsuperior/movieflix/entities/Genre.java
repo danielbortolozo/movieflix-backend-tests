6533dd99776417dc62dcfd2a5c525efc8a7dc6ae
Daniel Rocca Bortolozo <danielbortolozo@hotmail.com>
2021-04-02 15:35:04 -0300

    Implementando entity genero, e generoRepository.

---
+++ backend/src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -0,0 +1,36 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category")
+public class Genre implements Serializable {
+    private Long id;
+    private String name;
+
+    public Genre() {
+    }
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
